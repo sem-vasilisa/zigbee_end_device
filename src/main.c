8fb397893a25d4068066c713f66d345f904d5b78
Vasilisa Semyanikhina <you@example.com>
2026-07-31 13:35:15 +0200

    testing end device

--- src/main.c
+++ src/main.c
@@ -11,13 +11,24 @@
 LOG_MODULE_REGISTER(btz, LOG_LEVEL_INF);
 
 #define LIGHT_BULB_ENDPOINT 10
-#define LED_NODE DT_ALIAS(led0)
+// #define LED_NODE DT_ALIAS(led0)
 
 #define COORDINATOR_SHORT_ADDR 0x0000
 #define COORDINATOR_EP 10
+
+/* Parametry sterowane build-time (Kconfig aplikacji). Fallback na wypadek
+ * builda bez pliku Kconfig w katalogu aplikacji - wartosci jak wczesniej. */
+#ifndef CONFIG_ZB_LONG_POLL_INTERVAL_S
+#define CONFIG_ZB_LONG_POLL_INTERVAL_S 30
+#endif
+#ifndef CONFIG_ZB_REPORT_PERIOD_S
+#define CONFIG_ZB_REPORT_PERIOD_S 60
+#endif
+#define ZB_LONG_POLL_INTERVAL_MS (CONFIG_ZB_LONG_POLL_INTERVAL_S * 1000)
+#define ZB_REPORT_PERIOD_MS      (CONFIG_ZB_REPORT_PERIOD_S * 1000)
 zb_uint16_t coord_short_addr = COORDINATOR_SHORT_ADDR; /* coordinator address we need for communication */
 
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
+// static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
 
 struct zb_device_ctx{
     zb_zcl_basic_attrs_ext_t basic_attr;
@@ -48,17 +59,17 @@ ZB_DECLARE_LIGHT_BULB_EP(light_bulb_ep, LIGHT_BULB_ENDPOINT, light_bulb_clusters
 /* device context */
 ZBOSS_DECLARE_DEVICE_CTX_1_EP(light_bulb_ctx, light_bulb_ep);
 
-static void on_off_set_value(zb_bool_t value){
-    dev_ctx.on_off_attr.on_off = value;
-    if(value){
-        LOG_INF("LED IS OFF!");
-        gpio_pin_set_dt(&led, 1);
-    }
-    else{
-        LOG_INF("LED IS ON!");
-        gpio_pin_set_dt(&led, 0);
-    }
-}
+// static void on_off_set_value(zb_bool_t value){
+//     dev_ctx.on_off_attr.on_off = value;
+//     if(value){
+//         LOG_INF("LED IS OFF!");
+//         gpio_pin_set_dt(&led, 1);
+//     }
+//     else{
+//         LOG_INF("LED IS ON!");
+//         gpio_pin_set_dt(&led, 0);
+//     }
+// }
 
 static zb_uint8_t zcl_device_cb(zb_bufid_t bufid){
     zb_zcl_device_callback_param_t *p = ZB_BUF_GET_PARAM(bufid, zb_zcl_device_callback_param_t); /* returns a pointer to the data in the buffer */
@@ -70,16 +81,18 @@ static zb_uint8_t zcl_device_cb(zb_bufid_t bufid){
 
     zb_uint16_t cluster_id = p->cb_param.set_attr_value_param.cluster_id;
     zb_uint16_t attr_id = p->cb_param.set_attr_value_param.attr_id;
+    (void)cluster_id;
+    (void)attr_id;
 
-    if(cluster_id == ZB_ZCL_CLUSTER_ID_ON_OFF && attr_id == ZB_ZCL_ATTR_ON_OFF_ON_OFF_ID){
-        zb_uint8_t value = p->cb_param.set_attr_value_param.values.data8;
-        if(value){
-            on_off_set_value(ZB_TRUE);
-        }
-        else{
-            on_off_set_value(ZB_FALSE);
-        }
-    }
+    // if(cluster_id == ZB_ZCL_CLUSTER_ID_ON_OFF && attr_id == ZB_ZCL_ATTR_ON_OFF_ON_OFF_ID){
+    //     zb_uint8_t value = p->cb_param.set_attr_value_param.values.data8;
+    //     if(value){
+    //         on_off_set_value(ZB_TRUE);
+    //     }
+    //     else{
+    //         on_off_set_value(ZB_FALSE);
+    //     }
+    // }
     return ZB_FALSE;
 }
 
@@ -90,7 +103,7 @@ void zboss_signal_handler(zb_bufid_t bufid){
 
     switch(sig){
         case ZB_BDB_SIGNAL_DEVICE_FIRST_START:
-            LOG_INF("Joining network for the first time...");
+            // LOG_INF("Joining network for the first time...");
             bdb_start_top_level_commissioning(ZB_BDB_NETWORK_STEERING);
             break;
 
@@ -104,7 +117,7 @@ void zboss_signal_handler(zb_bufid_t bufid){
                 uint8_t channel = zb_get_current_channel();
                 uint16_t shortAddr = zb_get_short_address();
 
-                LOG_INF("Joined a network: PAN ID=0x%04X, Channel=%u, Short Addr=0x%04X", panId, channel, shortAddr);
+                // LOG_INF("Joined a network: PAN ID=0x%04X, Channel=%u, Short Addr=0x%04X", panId, channel, shortAddr);
             }
             else{
                 /* ZB network unit of time is BI(beacon interval), ZB devices use it to schedule operations. Beacons because zb in built on ieee802.15.4 which measures time in beacon intervals */
@@ -114,7 +127,7 @@ void zboss_signal_handler(zb_bufid_t bufid){
 
         /* if an end device leaves the zigbee network */
         case ZB_ZDO_SIGNAL_LEAVE:
-            LOG_INF("Reconnecting the network...");
+            // LOG_INF("Reconnecting the network...");
             ZB_SCHEDULE_APP_ALARM((zb_callback_t)bdb_start_top_level_commissioning, ZB_BDB_NETWORK_STEERING, ZB_MILLISECONDS_TO_BEACON_INTERVAL(1000)); /* after delay call this function */
             break;
 
@@ -133,7 +146,7 @@ static void send_test_report(zb_uint8_t param)
     zb_bufid_t bufid = zb_buf_get_out(); /* free memory buffer */
     if (!bufid) {
         LOG_ERR("No buffer available");
-        ZB_SCHEDULE_APP_ALARM(send_test_report, 0, ZB_MILLISECONDS_TO_BEACON_INTERVAL(60000));
+        ZB_SCHEDULE_APP_ALARM(send_test_report, 0, ZB_MILLISECONDS_TO_BEACON_INTERVAL(ZB_REPORT_PERIOD_MS));
         return;
     }
 
@@ -149,23 +162,23 @@ static void send_test_report(zb_uint8_t param)
 
     ZB_ZCL_FINISH_PACKET(bufid, ptr)
     ZB_ZCL_SEND_COMMAND_SHORT(bufid, coord_short_addr, ZB_APS_ADDR_MODE_16_ENDP_PRESENT, COORDINATOR_EP, LIGHT_BULB_ENDPOINT, ZB_AF_HA_PROFILE_ID, ZB_ZCL_CLUSTER_ID_ON_OFF, NULL); /* combines the packet and sends it */
-    LOG_INF("Sent test report"); /* locally we see that frame was sent */
-    ZB_SCHEDULE_APP_ALARM(send_test_report, 0, ZB_MILLISECONDS_TO_BEACON_INTERVAL(20000)); /* function repeats every minute */
+    // LOG_INF("Sent test report"); /* locally we see that frame was sent */
+    ZB_SCHEDULE_APP_ALARM(send_test_report, 0, ZB_MILLISECONDS_TO_BEACON_INTERVAL(ZB_REPORT_PERIOD_MS)); /* domyslnie 1min - function repeats every minute */
 }
 
 int main(void){
-    LOG_INF("Starting Zigbee Light Bulb (Sleepy End Device)");
-    gpio_pin_configure_dt(&led, GPIO_OUTPUT_INACTIVE); /* configure led gpio as output*/
+    // LOG_INF("Starting Zigbee Light Bulb (Sleepy End Device)");
+    // gpio_pin_configure_dt(&led, GPIO_OUTPUT_INACTIVE); /* configure led gpio as output and force it off */
     
     ZB_ZCL_REGISTER_DEVICE_CB((zb_callback_t)zcl_device_cb); /* tell zboss which function ahould be called as an event handler callback */
     ZB_AF_REGISTER_DEVICE_CTX(&light_bulb_ctx); /* register device context */
     app_clusters_attr_init(); /* attribute init function */
     
     zb_set_ed_timeout(ED_AGING_TIMEOUT_64MIN); /* set end device waiting timeout - if no reaction for 64 minutes -> end device is dead */
-    zb_set_keepalive_timeout(ZB_MILLISECONDS_TO_BEACON_INTERVAL(30000)); /* set a keepalive timeout - how often an end device contacts parent to say it's alive */
+    zb_set_keepalive_timeout(ZB_MILLISECONDS_TO_BEACON_INTERVAL(600000)); /* 10 mins - set a keepalive timeout - how often an end device contacts parent to say it's alive */
     zigbee_configure_sleepy_behavior(true); /* enable sleepy behavoir */
 
-    zb_zdo_pim_set_long_poll_interval(3000); /* how often an end device wakes up to ask a parent about a new message  */
+    zb_zdo_pim_set_long_poll_interval(ZB_LONG_POLL_INTERVAL_MS); /* domyslnie 30s - how often an end device wakes up to ask a parent about a new message  */
     zigbee_enable(); /* enable zigbee */
     ZB_SCHEDULE_APP_ALARM(send_test_report, 0, ZB_MILLISECONDS_TO_BEACON_INTERVAL(1000));
     k_sleep(K_FOREVER); /* sleep forever*/
